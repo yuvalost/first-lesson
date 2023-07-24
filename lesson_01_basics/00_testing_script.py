3ac13b2b540f92da9397d3ca85616b67e0e577de
yuvalost <yosy13378@gmail.com>
2023-07-24 16:36:29 +0300

    "after lesson 21.7

---
+++ lesson_01_basics/00_testing_script.py
@@ -0,0 +1,36 @@
+a = 10  # it is a variable with a name 'a'
+print(a)
+print(type(a))
+
+a = "yuval"
+print(a)
+print(type(a))
+
+a = 10.2
+print(a)
+print(type(a))
+
+# a = 10               a  -> 10014567fe
+# 1. 10 ->  2. a  -> 3. a = 10
+
+
+# a = "yuval"                 a  ->  243543feda
+# 1. "yuval"  ->  2. a ->  3. a = "yuval"
+
+
+my_list = [1, "yuval", 2, "bla bla"]
+print(my_list)
+print(type(my_list))
+
+print(type("yuval"))
+
+my_name = '''
+print("yuval ost")
+print(35)
+'''   # It is NOT a comment! It is a string
+
+print(my_name)
+
+"""
+Text
+"""
