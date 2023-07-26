96ff2dd66bc3864c2debe84980050f7600c26476
yuvalost <yosy13378@gmail.com>
2023-07-26 14:11:49 +0300

    second commit

---
+++ practice/01_practices.py
@@ -0,0 +1,22 @@
+print("Hello! welcome to John Smith Hospital! i have some questions")
+# name
+name = input('What is your name? ')
+if name == "john smith":
+    print("youre John Smith!!! i been waiting for that ass of yours! ")
+else:
+    print("suck my balls bitch you're not John Smith, wait...maybe your ass is still good")
+# age
+
+age = int(input('What is your age? '))
+if age <= 20:
+    print('we got a young bitch over here!!')
+else:
+    print('that some old ass, so hell no')
+# patient
+New_p = input('are you a new patient? (yes/no) ')
+if New_p == "yes":
+    print('so its a new ass!! for the hospital')
+else:
+    print('still going to hit that')
+
+
---
+++ practice/02_practice.py
@@ -0,0 +1,45 @@
+name = input('What is your name? ')
+f_color = input('What is your favoirt color? ')
+print(name + ' you choose '+f_color )
+
+
+# birthday exercise
+b_year = input('please eneter your birth year: ')
+if b_year >= "1991":
+    print('youre yonger than yuval')
+else:
+    print('youre older than yuval')
+# name print
+name = "yuval"
+
+print(name[0:1])
+print(name[1:2])
+print(name[2:3])
+print(name[3:4])
+print(name[4:5])
+
+number = 'hello'
+number += ' world'
+print(number)
+
+# number type
+
+a = 25
+A = 25.5
+
+print(type(a))
+print(type(A))
+
+# lens
+len('yuval')
+a = [1 , 2 , 3]
+if len(a) > 0:
+    print(" the list isnt empty")
+else:
+    print("the list is empty")
+A = [-1 , -2 , -3]
+if len(a) < 0:
+    print(" the list isnt empty")
+else:
+    print("the list is empty")
+
