bf2ef84e5e8e97b40248604506d9e23be2387f39
yuvalost <yosy13378@gmail.com>
2023-08-03 17:13:57 +0300

    "after lesson 31.7"

---
+++ practice/class_hw_31-7.py
@@ -0,0 +1,33 @@
+# person_age = int(input("what is your age: "))
+# plus = int(1)
+# print("next year youre going to be " (person_age) + 1)
+
+# number = int(input("enter the number: "))
+#
+# print("number multiplay by 2 is : ", int((number) * (2)))
+# print("number dividied by 2 is : ", int((number) / (2)))
+# print(int((number) / (2)))
+
+# number = float(input("enter the number: "))
+#
+# print("(number) in the power of 2: ", float((number ** 2)))
+
+# score_1, score_2,  = map(int, input("Enter 2 numbers: ").split())
+# print(score_1, score_2, score_1 + score_2, sep='\n')
+
+# score_1, score_2,  = map(float, input("Enter 2 numbers: ").split())
+# print(score_1 * score_2, 2*(score_1 + score_2), sep='\n')
+#
+# fire = float(input())
+#
+# print((fire - 32) * 5 / 9)
+
+# score_1, score_2, score_3, score_4 = map(float, input("Enter 4 numbers: ").split())
+# print((score_1 + score_2 +score_3 + score_4) / 4 , sep='\t')
+
+# a, b, c, d = map(float, input("Enter 4 grades: ").split())
+#
+# print(max(a, b, c , d))
+numbers = int(input("enter number: "))
+
+print(numbers - 1 , numbers , numbers + 1, sep='<')
