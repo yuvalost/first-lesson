de276c5cfc9bf24335ef49718c7c4aadac973971
yuvalost <yosy13378@gmail.com>
2023-08-07 17:42:38 +0300

    "after lesson 31.7"

--- lesson_01_variables/01_basics/00_testing_script.py
+++ lesson_01_variables/01_basics/00_testing_script.py
@@ -2,7 +2,7 @@ a = 10  # it is a variable with a name 'a'
 print(a)
 print(type(a))
 
-a = "Alex"
+a = "yuval"
 print(a)
 print(type(a))
 
@@ -14,18 +14,18 @@ print(type(a))
 # 1. 10 ->  2. a  -> 3. a = 10
 
 
-# a = "Alex"                 a  ->  243543feda
-# 1. "Alex"  ->  2. a ->  3. a = "Alex"
+# a = "yuval"                 a  ->  243543feda
+# 1. "yuval"  ->  2. a ->  3. a = "yuval"
 
 
-my_list = [1, "alex", 2, "bla bla"]
+my_list = [1, "yuval", 2, "bla bla"]
 print(my_list)
 print(type(my_list))
 
-print(type("Alex"))
+print(type("yuval"))
 
 my_name = '''
-print("Alexander Komanov")
+print("yuval ost")
 print(35)
 '''   # It is NOT a comment! It is a string
 
--- practice/02_practice.py
+++ practice/02_practice.py
@@ -1,86 +1,86 @@
-name = input('What is your name? ')
-f_color = input('What is your favoirt color? ')
-print(name + ' you choose '+f_color )
-
-
-# birthday exercise
-b_year = input('please eneter your birth year: ')
-if b_year >= "1991":
-    print('youre yonger than yuval')
-else:
-    print('youre older than yuval')
-# name print
-name = "yuval"
-
-print(name[0:1])
-print(name[1:2])
-print(name[2:3])
-print(name[3:4])
-print(name[4:5])
-
-number = 'hello'
-number += ' world'
-print(number)
-
-# number type
-
-a = 25
-A = 25.5
-
-print(type(a))
-print(type(A))
-
-# lens
-len('yuval')
-a = [1 , 2 , 3]
-if len(a) > 0:
-    print(" the list isnt empty")
-else:
-    print("the list is empty")
-A = [-1 , -2 , -3]
-if len(a) < 0:
-    print(" the list isnt empty")
-else:
-    print("the list is empty")
-
-print(bin(1))
-# f string test
-
-name = "yuval"
-a= "king"
-
-print(f' hello, {name}  {a}')
-
-# practice input
-
-name = input("write it please")
-age = input("write it please")
-print (f' answer :{name} {age}' )
-print (f" answer :{name} {age}" )
-# int practice and floot
-
-number_1 = int(input("write number1 please"))
-number_2 = int(input("write number2 please"))
-print(f' so.... {number_1} + {number_2} is {number_1 + number_2}')
-
-number_1 = float(input("write number1 please"))
-number_2 = float(input("write number2 please"))
-print(f' so.... {number_1} + {number_2} is {number_1 + number_2}')
-
-# split
-score_1 , score_2 = input("enter your score: ").split()
-
-Score_1 = float(score_1)
-Score_2 = float(score_2)
-
-
-
-print(score_1 , score_2)
-
-age1 , age2 = input("enter your 2 age numbers: ").split()
-
-print(age1 , age2)
-
-score_1, score_2 ,score_3 = map(int, input("enter 3 scores: ").split())
-
-print(score_1, score_2 ,score_3)
+# name = input('What is your name? ')
+# f_color = input('What is your favoirt color? ')
+# print(name + ' you choose '+f_color )
+#
+#
+# # birthday exercise
+# b_year = input('please eneter your birth year: ')
+# if b_year >= "1991":
+#     print('youre yonger than yuval')
+# else:
+#     print('youre older than yuval')
+# # name print
+# name = "yuval"
+#
+# print(name[0:1])
+# print(name[1:2])
+# print(name[2:3])
+# print(name[3:4])
+# print(name[4:5])
+#
+# number = 'hello'
+# number += ' world'
+# print(number)
+#
+# # number type
+#
+# a = 25
+# A = 25.5
+#
+# print(type(a))
+# print(type(A))
+#
+# # lens
+# len('yuval')
+# a = [1 , 2 , 3]
+# if len(a) > 0:
+#     print(" the list isnt empty")
+# else:
+#     print("the list is empty")
+# A = [-1 , -2 , -3]
+# if len(a) < 0:
+#     print(" the list isnt empty")
+# else:
+#     print("the list is empty")
+#
+# print(bin(1))
+# # f string test
+#
+# name = "yuval"
+# a= "king"
+#
+# print(f' hello, {name}  {a}')
+#
+# # practice input
+#
+# name = input("write it please")
+# age = input("write it please")
+# print (f' answer :{name} {age}' )
+# print (f" answer :{name} {age}" )
+# # int practice and floot
+#
+# number_1 = int(input("write number1 please"))
+# number_2 = int(input("write number2 please"))
+# print(f' so.... {number_1} + {number_2} is {number_1 + number_2}')
+#
+# number_1 = float(input("write number1 please"))
+# number_2 = float(input("write number2 please"))
+# print(f' so.... {number_1} + {number_2} is {number_1 + number_2}')
+#
+# # split
+# score_1 , score_2 = input("enter your score: ").split()
+#
+# Score_1 = float(score_1)
+# Score_2 = float(score_2)
+#
+#
+#
+# print(score_1 , score_2)
+#
+# age1 , age2 = input("enter your 2 age numbers: ").split()
+#
+# print(age1 , age2)
+#
+# score_1, score_2 ,score_3 = map(int, input("enter 3 scores: ").split())
+#
+# print(score_1, score_2 ,score_3)
