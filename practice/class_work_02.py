e8365853ff2c9b35fd714942dbc27fb7cd2333f6
yuvalost <yosy13378@gmail.com>
2023-08-08 20:02:21 +0300

    "after lesson 31.7"

---
+++ practice/class_work_02.py
@@ -0,0 +1,21 @@
+# num = int(input("please enter number: "))
+#
+#
+# if num > 0:
+#     print("True")
+# else:
+#     print("False")
+#
+# num_2 = int(input("please enter number: "))
+#
+# print(f'{num_2 > 0}')
+
+# num = int(input(" please enter number: "))
+#
+# print(num % 2 == 0)
+
+# num = int(input(" please enter number: "))
+# print(num % 6 == 0)
+
+num = int(input(" please enter number: "))
+print(num % 10 == 2)
