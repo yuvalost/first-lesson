89aa0a050d715b99df28bc6efaac0a8b0166fd2c
yuvalost <yosy13378@gmail.com>
2023-08-14 17:41:31 +0300

    "after lesson 31.7"

---
+++ lesson_01_variables/05_booleans/03_1_logical_operators_And.py
@@ -0,0 +1,17 @@
+# And
+print(f"True and True = {True and True}")
+print(f"4 > 3 and 5 > 4 = {4 > 3 and 5 > 4}")
+
+print(f"False and True = {False and True}")
+print(f"4 < 3 and 5 > 4 = {4 < 3 and 5 > 4}")
+
+print(f"False and False = {False and False}")
+print(f"True and False = {True and False}")
+
+print(f"True and True and True and False = {True and True and True and False}")
+print(f"False and True and True and False = {False and True and True and True and False}")
+
+number = 101
+
+print(number >= 10 and number <= 99)  # print(10 <= number <= 99)
+
