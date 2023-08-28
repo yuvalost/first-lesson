9feae744f545e45f4e14b2af5df323bdb6af7a9e
yuvalost <yosy13378@gmail.com>
2023-08-28 17:11:55 +0300

    "after lesson 31.7"

---
+++ .idea/part_02_list/01_basics/01_basics_list.py
@@ -0,0 +1,10 @@
+names = ["yuval", "alex","adva" ,"miley" ]
+
+print(f"{names = }")
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print("names [0] = ", names[0])
+print(type(names))
---
+++ part_01_variables/06_02_string_basics/class_tasks/practice.py
@@ -0,0 +1,26 @@
+# fruit = ['apple', 'banna', 'cherry']
+# fruit.pop(1)
+# fruit.insert(1, 'jim')
+# print(fruit)
+#
+# fruit = ['apple', 'banna', 'cherry']
+# for fruits in fruit:
+# #     print(f"frui is: {fruits}")
+# fruit_map = dict()
+# fruit_map = {1: 'apple', 2: 'banna', "yuval": '5678', 3: 'apple'}
+# print(fruit_map.values())
+# print(fruit_map.keys())
+#
+# numbers_2 = list(map(int, input("please enter number: " ).split()))
+# sum = 0
+# num_int = len(numbers_2)
+# for num in numbers_2:
+#     sum += num
+# print(sum/num_int)
+#
+# numbers = list(map(int, input("please enter number: " ).split()))
+# print(float(sum(numbers) / len(numbers)))
+
+number_list = list(map(int, input("please enter number: " ).split()))
+print("777" in number_list)
+
