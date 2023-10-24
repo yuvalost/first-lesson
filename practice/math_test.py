ae3d423ec23d2b886cce78ac37a60dc51fe08878
yuvalost <yosy13378@gmail.com>
2023-10-24 16:16:03 +0300

    second commit

--- practice/math_test.py
+++ practice/math_test.py
@@ -14,8 +14,7 @@ def generate_problem():
     equation_formation = str(left) + " " + math_symbol + " " + str(right)
     answer = eval(equation_formation)
     return equation_formation, answer
-
-def play_game:
+def play_game():
     wrong = 0
     input("press enter or any key (i dont care..)")
     print("-" * 100)
@@ -36,29 +35,13 @@ def play_game:
     total_time = end_time - start_time
     print("-" * 100)
     print("Nice work you finished in", round(total_time), "seconds!")
+while True:
+    play_game()
+    anoter_try = input("Nice lets beat that time?y/n  ")
+    if anoter_try != "y":
+        print("i will take that as no....goodbye")
+        break
 
 
-def again():
-anoter_try = input("Nice lets beat that time? y/n")
-if anoter_try == "y":
-    wrong = 0
-    start_time = time.time()
 
-    for i in range(total_problems):
-        equation_formation, answer = generate_problem()
-        while True:
-            user_guess = input("problem #" + str(i + 1) + ": " + equation_formation + " = ")
-
-            if user_guess == str(answer):
-                break
-
-            wrong += 1
-            again()
-
-    end_time = time.time()
-    total_time = end_time - start_time
-    print("-" * 100)
-    print("Nice work you finished in", round(total_time), "seconds!")
-else:
-    print("i will take that as no....goodbye")
 
