8b0f23e03fdb18865ae223a1c316efe96a5d0b4b
yuvalost <yosy13378@gmail.com>
2023-10-25 12:11:11 +0300

    second commit

--- practice/side_projects/harry_potter_test.py
+++ practice/side_projects/harry_potter_test.py
@@ -11,65 +11,59 @@ house_scores = {'Gryffindor': 0, 'Ravenclaw': 0, 'Hufflepuff': 0, 'Slytherin': 0
 
 
 questions = [
-    "1. What is your approach to facing danger? "
+    "What is your approach to facing danger? "
     
     "a) I confront it head-on.\n"
     "b) I analyze the situation carefully.\n"
     "c) I seek help and collaborate with others\n"
     "d) I use cunning and strategy to avoid it\n",
 
-    "2. What is your approach to facing danger? \n"
+    "What is your approach to facing danger? \n"
     
     "a) Adventures and heroic tales..\n"
     "b) Mysteries and puzzles.\n"
     "c) Heartwarming stories about friendship.\n"
     "d) Books about achieving power and success.\n",
 
-    "3. You have a choice between four Magic items.\n" 
-    
-    "a) A sword that only the worthy can pull from a stone.\n"
-    "b) A riddle-locked chest with untold secrets inside.\n"
-    "c) A lantern that leads you to the friends you seek.\n"
-    "d) A cloak that makes you invisible.\n",
-
-    "4. How do you respond to someone who wronged you or a friend?\n"
+    "How do you respond to someone who wronged you or a friend?\n"
     
     "a) I confront them directly and demand an apology.\n"
     "b) I try to understand their motives and resolve the issue calmly.\n"
     "c) I forgive and forget, valuing harmony and kindness.\n"
     "d) I seek revenge or find a way to outsmart them.\n",
 
-    "5. What's your favorite magical creature?\n"
+    "What's your favorite magical creature?\n"
+    
     "a) Phoenix\n"
     "b) Thestral\n"
     "c) Hippogriff\n"
     "d) Basilisk.",
 
-    "6. Which subject at Hogwarts would you be most excited to study?\n"
+    "Which subject at Hogwarts would you be most excited to study?\n"
+    
     "a) Defense Against the Dark Arts\n"
     "b) Potions and Alchemy\n"
     "c) Care of Magical Creatures\n"
     "d) Ancient Runes.",
 
-    "7. What quality do you value most in others?\n"
+    "What quality do you value most in others?\n"
+    
     "a) Bravery\n"
     "b) Intelligence\n"
     "c) Loyalty\n"
     "d) Ambition.",
 
-    "8. In a magical duel, which spell would you use?\n"
-    "a) Expelliarmus\n"
-    "b) Alohomora\n"
-    "c) Patronus Charm\n"
-    "d) Imperius Curse.",
 
-    "9. Choose a word that best describes you:\n"
+
+    "Choose a word that best describes you:\n"
+    
     "a) Daring\n"
     "b) Curious\n"
     "c) Loyal\n"
     "d) Ambitious.",
 
-    "10. Which Hogwarts house do you secretly admire the most?\n"
+    "Which Hogwarts house do you secretly admire the most?\n"
+    
     "a) Gryffindor\n"
     "b) Ravenclaw\n"
     "c) Hufflepuff\n"
@@ -85,11 +79,12 @@ def ask_question(question):
         house_scores[house] += answer_points[answer][house]
 
 
-name = input("please enter your name: ")
-print(f"Welcome {name} to the Hogwarts House Sorting Quiz!")
-print("Answer the following questions to discover which Hogwarts house you belong to.")
+
 
 while True:
+    name = input("please enter your name: ")
+    print(f"Welcome {name} to the Hogwarts House Sorting Quiz!")
+    print("Answer the following questions to discover which Hogwarts house you belong to.")
     for question in questions:
         ask_question(question)
 
