5d741ed92402e2bf25c00b84b901d08c0e7d1cb4
yuval <yosy13378@gmail.com>
2023-12-19 08:43:28 +0200

    dasd

--- part_12_oop/01_basic_class.py
+++ part_12_oop/01_basic_class.py
@@ -8,12 +8,17 @@ class MyCar:
     def go_faster(self, max_speed: int):
         print("Accelerate up to ", max_speed)
 
+    def happy_meal(self, what_meal_do_you_want: input()):
+        print("what happy meal you want? ", what_meal_do_you_want)
 
-tesla = MyCar()
-print(type(tesla))
 
-print(tesla.year)
-print(tesla.brand)
-print(type("Alex"))
-tesla.car_drive()
-tesla.go_faster(230)
+#
+# tesla = MyCar()
+# print(type(tesla))
+#
+# print(tesla.year)
+# print(tesla.brand)
+# print(type("Alex"))
+# tesla.car_drive()
+# tesla.go_faster(230)
+tesla.happy_meal(chips)
