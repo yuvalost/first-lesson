fca9b03dbe720f1a49f65303a7ed2e34d11a044e
yuvalost <yosy13378@gmail.com>
2024-01-04 17:53:35 +0200

    "after lesson 8.31"

---
+++ practice/basic_app.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, types
+
+
+
+
+BOT_TOKEN = "6447680509:AAFtZP4lzes4DdrpEpvDEk8EVkx5iYK3Cgk"
+boto=Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+def main():
+    pass
+
+
